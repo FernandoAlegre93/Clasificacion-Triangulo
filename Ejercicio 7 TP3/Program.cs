4c366475b5a1ef94e977060131dc4d21599f79ba
Fernando <feralegre777@hotmail.com>
2021-06-12 19:00:39 -0300

    Subiendo

---
+++ Ejercicio 7 TP3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_7_TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B ,C;
+           
+                Console.WriteLine("Ingrese el Lado A");
+                A = Convert.ToInt32 (Console.ReadLine());
+                Console.WriteLine("Ingrese el Lado B");
+                B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el Lado C");
+                C = Convert.ToInt32(Console.ReadLine());
+
+                if (A == B & B == C & A == C)
+                {
+                    Console.WriteLine("El triangulo es equilatero");
+                }
+                else if
+                    (A != B & B != C & A != C)
+                {
+                    Console.WriteLine("El triangulo es Escaleno");
+                }
+                else if
+                    (A != B || B != C || A != C)
+                {
+                    Console.WriteLine("El triangulo es Isoceles");
+                }
+
+                
+    
+         Console.ReadKey();
+
+
+        }
+    }
+}
